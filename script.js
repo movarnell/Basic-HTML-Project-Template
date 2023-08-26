f4222cf408ba946bc3d6044c3de588d25a8ebb54
Michael Varnell <movarnell@gmail.com>
2023-08-25 23:29:59 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//REVIEW This is a basic template to get you started with your own script.
+//TODO You can delete everything in this file and start from scratch.
+//INFO If you use the 'Live Server' extension in VS Code, you can see your changes in real time in the browser. If it is already installed just right click on the index.html file and select 'Open with Live Server'. Then you can open the console in the browser to see the output of your script.
+console.log("Hello World!");
